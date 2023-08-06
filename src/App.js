b520351f95cac210549bafdae11a76db3e804c95
Parv-gugnani <9765.parv@gmail.com>
2023-08-06 21:04:44 +0530

    book show

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
 
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter(() => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
   const createBook = (title) => {
-    books.push({ id: 123, title: title });
-    setBooks(books);
+    const updatedBooks = [...books, { id: Math.random() * 9999, title }];
+    setBooks(updatedBooks);
   };
 
   return (
-    <div>
-      {books.length}
+    <div className="app">
+      <BookList books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
