ae09cd6e4f8856c87f19ca7011273d0c7811e8dc
Parv-gugnani <9765.parv@gmail.com>
2023-08-13 17:21:13 +0530

    provider

--- src/context/books.js
+++ src/context/books.js
@@ -1,5 +1,22 @@
-import { createContext, createElement } from "react";
+import { createContext, useState } from "react";
 
 const BooksContext = createElement();
 
+function Provider({ children }) {
+  const [count, children] = useState(5);
+
+  const valueToShare = {
+    count: count,
+    incrementCount: () => {
+      setCount(count + 1);
+    },
+  };
+  return (
+    <BooksContext.Provider value={valueToShare}>
+      {children}
+    </BooksContext.Provider>
+  );
+}
+
+export { Provider };
 export default BooksContext;
