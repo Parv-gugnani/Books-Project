30f64188fe9b2093e9d9143fb0ee1299c9202a73
Parv-gugnani <9765.parv@gmail.com>
2023-08-14 12:49:09 +0530

    delete

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,19 +3,12 @@ import BooksContext from "../context/books";
 import BookShow from "./BookShow";
 
 function BookList({ books, onDelete, onEdit }) {
-  const { count, incrementCount } = useContext(BooksContext);
   const renderedBooks = books.map((book) => {
     return (
       <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
     );
   });
-  return (
-    <div className="book-list">
-      {count}
-      <button onClick={incrementCount}>Click</button>
-      {renderedBooks}
-    </div>
-  );
+  return <div className="book-list">{renderedBooks}</div>;
 }
 
 export default BookList;
--- src/context/books.js
+++ src/context/books.js
@@ -3,19 +3,7 @@ import { createContext, useState } from "react";
 const BooksContext = createElement();
 
 function Provider({ children }) {
-  const [count, children] = useState(5);
-
-  const valueToShare = {
-    count: count,
-    incrementCount: () => {
-      setCount(count + 1);
-    },
-  };
-  return (
-    <BooksContext.Provider value={valueToShare}>
-      {children}
-    </BooksContext.Provider>
-  );
+  return <BooksContext.Provider value={{}}>{children}</BooksContext.Provider>;
 }
 
 export { Provider };
